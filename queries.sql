6f07e34ba28816ed93a83bae35862b6724963be0
MangatH <111387025+MangatH@users.noreply.github.com>
2022-10-09 21:48:49 +0530

    Adding queries.sql

--- queries.sql
+++ queries.sql
@@ -7,11 +7,14 @@ AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 select * from retirement_info;
 
--- Number of employees retiring
-SELECT COUNT(first_name)
+-- Number of employees retiring and creating new table
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
 
 -- Employees born in 1952
 SELECT first_name, last_name
